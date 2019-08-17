83e16244ddaea38502f9b914425957fd206cff11
Grigory Tantsevov <tantsevov@gmail.com>
2019-08-18 01:45:35 +0300

    tests

--- edge/skelet_dnn/main.py
+++ edge/skelet_dnn/main.py
@@ -2,8 +2,8 @@ from __future__ import division
 import cv2
 import numpy as np
 
-protoFile = "hand/pose_deploy.prototxt"
-weightsFile = "hand/pose_iter_102000.caffemodel"
+protoFile = "skelet_dnn/hand/pose_deploy.prototxt"
+weightsFile = "skelet_dnn/hand/pose_iter_102000.caffemodel"
 nPoints = 22
 POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [0, 5], [5, 6], [6, 7], [7, 8], [0, 9], [9, 10], [10, 11], [11, 12],
               [0, 13], [13, 14], [14, 15], [15, 16], [0, 17], [17, 18], [18, 19], [19, 20]]
--- edge/test.py
+++ edge/test.py
@@ -8,6 +8,7 @@ import edge.canny_filter.canny_edge_detector as ced
 
 from edge.canny_filter.utils.utils import rgb2gray
 from edge.opencv_filter.opencv import opencv, paln_point, skel
+from edge.skelet_dnn.main import skeleton
 from edge.sobel import Sobel
 from edge.prewitt import Prewitt
 from edge.roberts import Roberts
@@ -15,60 +16,70 @@ from edge.roberts import Roberts
 nothing_image_path = "../data/dataset/asl-alphabet/asl_alphabet_test/nothing_test.jpg"
 
 
+def canny_func(frame):
+    img = rgb2gray(frame)
+    detector = ced.cannyEdgeDetector([img], sigma=1.0, kernel_size=5, lowthreshold=0.01, highthreshold=0.07,
+                                     weak_pixel=100)
+    img_final = detector.detect()[0]
+    if img_final.shape[0] == 3:
+        img_final = img_final.transpose(1, 2, 0)
+    gray()
+    return img_final
+
 class MyTestCase(unittest.TestCase):
-    def test_sobel(self):
-        print(f'SOBEL')
-        dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
-        output_path = "sobel"
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        for image in os.listdir(dataset_path):
-            print(f'Processing {image}')
-            out_name = image
-            sobel = Sobel(os.path.join(dataset_path, image))
-            sobel.save_im(os.path.join(output_path, out_name))
+    # def test_sobel(self):
+    #     print(f'SOBEL')
+    #     dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
+    #     output_path = "sobel"
+    #     if not os.path.exists(output_path):
+    #         os.makedirs(output_path)
+    #     for image in os.listdir(dataset_path):
+    #         print(f'Processing {image}')
+    #         out_name = image
+    #         sobel = Sobel(os.path.join(dataset_path, image))
+    #         sobel.save_im(os.path.join(output_path, out_name))
 
-    def test_prewitt(self):
-        print(f'PREWITT')
-        dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
-        output_path = "prewitt"
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        for image in os.listdir(dataset_path):
-            print(f'Processing {image}')
-            out_name = image
-            sobel = Prewitt(os.path.join(dataset_path, image))
-            sobel.save_im(os.path.join(output_path, out_name))
+    # def test_prewitt(self):
+    #     print(f'PREWITT')
+    #     dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
+    #     output_path = "prewitt"
+    #     if not os.path.exists(output_path):
+    #         os.makedirs(output_path)
+    #     for image in os.listdir(dataset_path):
+    #         print(f'Processing {image}')
+    #         out_name = image
+    #         sobel = Prewitt(os.path.join(dataset_path, image))
+    #         sobel.save_im(os.path.join(output_path, out_name))
 
-    def test_roberts(self):
-        print(f'ROBERTS')
-        dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
-        output_path = "roberts"
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        for image in os.listdir(dataset_path):
-            print(f'Processing {image}')
-            out_name = image
-            sobel = Roberts(os.path.join(dataset_path, image))
-            sobel.save_im(os.path.join(output_path, out_name))
+    # def test_roberts(self):
+    #     print(f'ROBERTS')
+    #     dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
+    #     output_path = "roberts"
+    #     if not os.path.exists(output_path):
+    #         os.makedirs(output_path)
+    #     for image in os.listdir(dataset_path):
+    #         print(f'Processing {image}')
+    #         out_name = image
+    #         sobel = Roberts(os.path.join(dataset_path, image))
+    #         sobel.save_im(os.path.join(output_path, out_name))
 
-    def test_canny(self):
-        print("CANNY")
-        dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
-        output_path = "canny"
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-        for image in os.listdir(dataset_path):
-            print(f'Processing {image}')
-            img = mpimg.imread(os.path.join(dataset_path, image))
-            img = rgb2gray(img)
-            detector = ced.cannyEdgeDetector([img], sigma=1.0, kernel_size=5, lowthreshold=0.01, highthreshold=0.07,
-                                             weak_pixel=100)
-            img_final = detector.detect()[0]
-            if img_final.shape[0] == 3:
-                img_final = img_final.transpose(1, 2, 0)
-            gray()
-            imsave(os.path.join(output_path, image), img_final)
+    # def test_canny(self):
+    #     print("CANNY")
+    #     dataset_path = "../data/dataset/asl-alphabet/asl_alphabet_test"
+    #     output_path = "canny"
+    #     if not os.path.exists(output_path):
+    #         os.makedirs(output_path)
+    #     for image in os.listdir(dataset_path):
+    #         print(f'Processing {image}')
+    #         img = mpimg.imread(os.path.join(dataset_path, image))
+    #         img = rgb2gray(img)
+    #         detector = ced.cannyEdgeDetector([img], sigma=1.0, kernel_size=5, lowthreshold=0.01, highthreshold=0.07,
+    #                                          weak_pixel=100)
+    #         img_final = detector.detect()[0]
+    #         if img_final.shape[0] == 3:
+    #             img_final = img_final.transpose(1, 2, 0)
+    #         gray()
+    #         imsave(os.path.join(output_path, image), img_final)
 
     def test_opencv(self):
         print("OPENCV")
@@ -97,23 +108,29 @@ class MyTestCase(unittest.TestCase):
         for image in os.listdir(dataset_path):
             print(f'Processing {image}')
             orig = mpimg.imread(os.path.join(dataset_path, image))
-            canny = mpimg.imread(os.path.join("canny", image))
-            roberts = mpimg.imread(os.path.join("roberts", image))
-            prewitt = mpimg.imread(os.path.join("prewitt", image))
-            sobel = mpimg.imread(os.path.join("sobel", image))
-            openc = mpimg.imread(os.path.join("opencv", image))
+            canny = canny_func(orig)
+            roberts = Roberts(os.path.join(dataset_path, image))
+            prewitt = Prewitt(os.path.join(dataset_path, image))
+            sobel = Sobel(os.path.join(dataset_path, image))
+            openc = opencv(orig)
+            skek_opencv = skel(openc.copy())
+            skel_dnn = skeleton(orig.copy())
             figure(figsize=(20, 20))
-            subplot(3, 2, 1, title="canny", xticks=[], yticks=[])
+            subplot(3, 3, 1, title="canny", xticks=[], yticks=[])
             imshow(canny, "gray")
-            subplot(3, 2, 2, title="roberts", xticks=[], yticks=[])
-            imshow(roberts, "gray")
-            subplot(3, 2, 3, title="prewitt", xticks=[], yticks=[])
-            imshow(prewitt, "gray")
-            subplot(3, 2, 4, title="sobel", xticks=[], yticks=[])
-            imshow(sobel, "gray")
-            subplot(3, 2, 5, title="opencv", xticks=[], yticks=[])
+            subplot(3, 3, 2, title="roberts", xticks=[], yticks=[])
+            imshow(roberts.sobelIm, "gray")
+            subplot(3, 3, 3, title="prewitt", xticks=[], yticks=[])
+            imshow(prewitt.prewittIm, "gray")
+            subplot(3, 3, 4, title="sobel", xticks=[], yticks=[])
+            imshow(sobel.sobelIm, "gray")
+            subplot(3, 3, 5, title="opencv", xticks=[], yticks=[])
             imshow(openc, "gray")
-            subplot(3, 2, 6, title="original", xticks=[], yticks=[])
+            subplot(3, 3, 6, title="skel_openv", xticks=[], yticks=[])
+            imshow(skek_opencv)
+            subplot(3, 3, 7, title="skel_dnn", xticks=[], yticks=[])
+            imshow(skel_dnn)
+            subplot(3, 3, 8, title="original", xticks=[], yticks=[])
             imshow(orig)
             savefig(os.path.join(output_path, image))
 
